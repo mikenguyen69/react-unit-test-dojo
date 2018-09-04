2b57c53a0a9ed8d6fc5dcda333a6f75829072d63
Mike Nguyen <nguyenduchung0609@gmail.com>
2018-09-05 06:27:40 +0800

    clean up
    hello world to be cleaned to focus on only test expectation

--- src/__tests__/hello.world.test.js
+++ src/__tests__/hello.world.test.js
@@ -1,26 +1,22 @@
-describe('HelloWorld', () => {
+const React = require('react')
 
-    const React = require('react')
-    const TestUtils = require('react-dom/test-utils')
-    
-    class HelloWorld extends React.Component {
-        render() {
-            return <div>{this.props.children}</div>
-        }
+class HelloWorld extends React.Component {
+    render() {
+        return <div>{this.props.children}</div>
     }
+}
+
+describe('HelloWorld', () => {
+    const TestUtils = require('react-dom/test-utils')
+    let hello = TestUtils.renderIntoDocument(<HelloWorld>Hello Bros</HelloWorld>)
 
     it('has props', (done) => {
-        
-        let hello = TestUtils.renderIntoDocument(<HelloWorld>Hello Bros</HelloWorld>)
-
         expect(hello.props).toBeDefined()
         console.log('Myhello props: ', hello.props)
         done()
     })
 
     it('has a div', (done) => {
-        let hello = TestUtils.renderIntoDocument(<HelloWorld>Hello Bros</HelloWorld>)
-
         let div = TestUtils.scryRenderedDOMComponentsWithTag(hello, 'div').length
         expect(div).toBe(1)
         console.log('found this many divs: ', div)
